74b368c5d4bbe9f62ab7eaaf5b0c2b8518f266b4
Crampustallin <muha.br.ba@gmail.com>
2024-02-21 23:55:48 +0600

    styles initialized

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, 
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader' 
-        }
-      },
-      {
-        test: /\.scss$/, 
-        use: [
-          MiniCssExtractPlugin.loader, 
-          'css-loader', 
-          'sass-loader' 
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i, 
-        type: 'asset/resource' 
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ 
-      filename: 'styles.css'
-    })
-  ],
-  resolve: {
-    extensions: ['.js', '.scss'] 
-  }
+	entry: './src/index.ts', 
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/, 
+				exclude: /node_modules/,
+				use: {
+					loader: 'ts-loader' 
+				}
+			},
+			{
+				test: /\.s[ac]ss$/, 
+				use: [
+					'style-loader',
+					'css-loader', 
+					'sass-loader' 
+				]
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i, 
+				type: 'asset/resource' 
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin()
+	],
+	resolve: {
+		extensions: ['.tsx','.ts','.js', '.scss'] 
+	}
 };
 
