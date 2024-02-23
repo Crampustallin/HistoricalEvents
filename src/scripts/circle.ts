6d9130fa3dcd51f7ad84fc8af6baf00c287f6945
Crampustallin <muha.br.ba@gmail.com>
2024-02-23 18:12:07 +0600

    next and prev buttons added

--- src/scripts/circle.ts
+++ src/scripts/circle.ts
@@ -1,16 +1,23 @@
 import gsap from 'gsap'
 
-let wheel:HTMLElement | null = document.querySelector(".circle");
+let circle:HTMLElement | null = document.querySelector(".circle");
 let dots: HTMLElement[] = gsap.utils.toArray<HTMLElement>(".dots");
+let dateList: HTMLElement[] = gsap.utils.toArray<HTMLElement>(".date-list");
 let slice = 360 / dots.length;
-let currentDot:HTMLElement | null = document.querySelector(".active"); // keep track of last clicked card so we can put it back
+let curRotation = 0;
+let currentDot:HTMLElement | null = document.querySelector("div .active");
+const prevBtn = document.getElementById("prev-button");
+const nextBtn = document.getElementById("next-button");
 
+let updateButtonState = (nextDot : HTMLElement) => {
+	prevBtn?.setAttribute("disabled", `${!nextDot?.nextElementSibling}`);
+	nextBtn?.setAttribute("disabled", `${!nextDot?.previousElementSibling}`);
+}
 
 function setup() {
-	let radius: number = wheel?.offsetWidth ?? 0;
+	let radius: number = circle?.offsetWidth ?? 0;
 	let DEG2RAD: number = Math.PI / 180;
 	radius /= 2;
-	let center: number = radius;
 	gsap.set(dots, {
 		x: i => radius * Math.sin(i * slice * DEG2RAD),
 		y: i => radius * Math.cos(i * slice * DEG2RAD),
@@ -18,41 +25,91 @@ function setup() {
 		xPercent: -50,
 		yPercent: -50
 	});
+	gsap.set(dateList, {
+		ease: "power1.in"
+	});
 }
 
 setup();
 
-  const rotationBtnComplete = () => {
-	  //let dot = document.querySelector('.active');
-	  //dot?.classList.remove('active');
+
+let removeCurrentActive = () => {
+	if(currentDot)
+		{
+			currentDot.classList.remove('active');
+			currentDot.firstElementChild?.classList.remove('active');
+		}
+}
+
+  const rotationBtnComplete = (dot: HTMLElement | null | undefined) => {
+	  console.log(dot);
+	  if(dot) {
+		  dot.classList.add('active');
+		  dot.firstElementChild?.classList.add('active');
+		  currentDot = dot;
+	  }
 };
 
 let onClickCard = (event: MouseEvent) => {
 	let dot = event.target as HTMLElement;
-	let style = dot?.getAttribute('style');
-	let deg: number = 0;
-	if(style) {
-		let s = style.match(/rotate\((\d+)deg\)/);
-		if(s && s[1]) {
-			deg = Number.parseInt(s[1]);
+	if(dot && currentDot !== dot)
+		{
+			let style = dot?.getAttribute('style');
+			let deg: number = 0;
+
+			if(style) {
+				let s = style.match(/rotate\((\d+)deg\)/);
+				if(s && s[1]) {
+					deg = Number.parseInt(s[1]);
+				}
+			}
+			curRotation = deg;
+
+			removeCurrentActive();
+
+			gsap.to(circle, {
+				duration: 0.75,
+				ease: "none",
+				rotation:  deg - 150, 
+				overwrite: "auto",
+			}).then(() => rotationBtnComplete(dot));
+			updateButtonState(dot);
 		}
-	}
-	if(currentDot) {
-		currentDot.classList.remove('active');
-	}
-	dot.classList.add('active');
-	currentDot = dot;
-	gsap.to(wheel, {
-		duration: 0.75,
-		ease: "none",
-		rotation:  deg - 150, 
-		overwrite: "auto",
-		onComplete: rotationBtnComplete,
-	});
 };
 
 
 dots.forEach((dot) => dot.addEventListener("click", onClickCard));
 
+if(prevBtn) {
+	prevBtn.addEventListener("click", () => {
+		if(prevBtn.getAttribute('disabled') !== 'true')
+			{
+				let prevDot = currentDot?.nextElementSibling as HTMLElement;
+				removeCurrentActive();
+				curRotation += slice;
+				gsap.to(circle, {
+					duration: 0.25,
+					ease: "power1.inOut",
+					rotation: curRotation - 150,
+				}).then(() => rotationBtnComplete(prevDot));
+				updateButtonState(prevDot);
+			}
+	});
+}
 
-
+if(nextBtn) {
+	nextBtn.addEventListener("click", () => {
+		if(nextBtn.getAttribute('disabled') !== 'true')
+			{
+				let nextDot = currentDot?.previousElementSibling as HTMLElement;
+				removeCurrentActive();
+				curRotation -= slice;
+				gsap.to(circle, {
+					duration: 0.25,
+					ease: "power1.inOut",
+					rotation: curRotation - 150,
+				}).then(() => rotationBtnComplete(nextDot));
+				updateButtonState(nextDot);
+			}
+	});
+}
